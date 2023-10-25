e6cb5b2ffa281ace458b361e65644acc5252c86d
raidas <mrraidas@gmail.com>
2023-10-25 20:40:56 +0300

    update

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 '''
-https://www.youtube.com/watch?v=CEUa1JgKcp0&ab_channel=VernierScienceEducation
+YT video to help understand the measurement process: https://youtu.be/CEUa1JgKcp0?si=u4uqtjKKAl3LA62J
 
-Noteworthy observations:
-
-    ▪ Instead of scaled raw absorbance data at each wavelength, do % change in between subsequent wavelengths.
-    This completely does not work and I've no understanding why. Does absorbance intercept manner that much?
-    spectra_ = spectra_.pct_change(axis=1).drop(0, axis=1)
-
-    ▪ Plot absorbance (y) vs concentration (x) at each wavelength. Expect it ta have a ~linear relationship?
-    Why there's no visible relationship..?
+Notes:
 
     ▪ Using ridge regression (regularize by keeping the squares of coefficients as low as possible), we can see which
-    wavelengths are most used by the model to predict y. Some ranges are more useful than other. e.g. ~2110 and ~2300.
-    See ridge_feature_importance.py for visualization.
+    wavelengths are most used by the model to predict y. Some ranges are more useful than others. e.g. ~2110 and ~2300.
+    Run ridge_feature_importance.py for visualization.
 
-    ▪ Currently, the only way to come to reasonable results is to either use PCA or regularize heavily to reduce X.
-    RandomForest, while not the best algo for this, could be used to pick features. Not now as not enough data points.
-
-    ▪ Too little data for RNN? No. There's no signal in the sequence of absorptivity across different wavelengths.
-    The signal lies in specific wavelengths, not the sequence.
+    ▪ To get reasonable results must either use PCA or regularize heavily to reduce X dim. RandomForest,
+    while not the best algo for this, could be used to pick features. Does not work as features are too
+    similar and signal is too hidden.
 
     ▪ How do I interpret PCA that is done across the wavelengths? Specific PCA components shows very high (~70 to 95%)
-    correlation with target concentration.
+    correlation with target concentration. Is it just a coincidence that 4th component works so well, while the rest
+    are not adding much to the signal..? Using PCA's 4th component as a single feature yields 0.91 R2 using LR.
 
-TODO: try PCA with polynomials?
+    ▪ Too little data for RNN? No. There's no signal in the sequence of absorptivity across different wavelengths.
+    The signal lies in specific wavelengths, not the sequence. Check out the X (after post-process) vs Y plot -
+    if some ranges are identical - there's no predicting power in these. The signal lies in ranges, where each
+    sample diverge from the rest and not overlap. Transformer should work well..?
 
-Best models:
-PCA(5) + Poly(2) + Ridge    - 0.924
-PCA(80) + CNN               - 0.922
+    ▪ After data processing, X samples (absorption at different wavelengths across measurements) are nearly identical.
+    Must engineer new features that would better differentiate each sample and amplify the differences. This would prob
+    yield best model results - how do we engineer new features that amplify differences at specific wavelengths?
+    PCA does it for us, but can we come up with even better features?
+        - Divide absorptions by max / min - spectra_.div(spectra_.max(axis=1), axis=0) -> [1, 0.98, 0.97 ...]
+
+    ▪ Instead of scaled raw absorbance data at each wavelength, do % change in between subsequent wavelengths.
+    This completely does not work and I've no understanding why. Does absorbance intercept manner that much? No.
+    But this method does not amplify the slight deviations in absorbance that actually matter for concentration.
+    spectra_ = spectra_.pct_change(axis=1).drop(0, axis=1)
+
+    ▪ For now it's not about the model architecture. It's about intuitive understanding of the data and
+    why PCA works so well, while other methods yield little results. How do I explain and visualize this?
+
+Model results:                            test set R-squared
+    Linear + PCA(20) + manual max scale - 0.943
+    Linear + PCA(20)                    - 0.913
+    Ridge (alpha=0.0015)                - 0.894
+    PCA(5) + Poly(2) + Ridge            - 0.924
+    PCA(80) + CNN                       - 0.922
+    CNN no PCA                          - ~0.8
+    RNN - couldn't get it to work properly
 
 '''
 
@@ -37,6 +51,7 @@ import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn import preprocessing
 from sklearn import linear_model
+from sklearn.svm import SVR
 from sklearn.decomposition import PCA
 from sklearn import feature_selection
 from sklearn.neural_network import MLPRegressor
@@ -68,6 +83,7 @@ spectra.set_index('wavelength').plot(legend=False)
 
 # plot absorbance (y) vs concentration (x) at each wavelength
 # shouldn't we expect it ta have a ~linear relationship?
+# as in the video: https://youtu.be/CEUa1JgKcp0?si=wwjOgg2gzEaqHZPi&t=272
 plt.figure()
 for i in range(1, spectra.shape[0]):
     plt.scatter(
@@ -81,6 +97,7 @@ for i in range(1, spectra.shape[0]):
 # define x and y
 y = concentration['concentration_lactate']
 spectra_ = spectra.drop('wavelength', axis=1).transpose()
+spectra_ = spectra_.div(spectra_.max(axis=1), axis=0)
 
 # split data
 X_train, X_test, y_train, y_test = train_test_split(spectra_, y, test_size=0.2, random_state=42)
@@ -88,13 +105,10 @@ X_train, X_test, y_train, y_test = train_test_split(spectra_, y, test_size=0.2, 
 # define model
 model = Pipeline([
     # ('scale_each_sample', preprocessing.Normalizer()),
-    ('scale', preprocessing.StandardScaler()),
-    # ('feature_selection', feature_selection.RFE(linear_model.Ridge(alpha=0.01), n_features_to_select=150)),
+    # ('scale', preprocessing.StandardScaler()),
     ('reduce dims', PCA(20)),
     # ('add_polynomials', preprocessing.PolynomialFeatures(2, include_bias=False)),
-    # ('clf', RandomForestRegressor(n_estimators=10, min_samples_leaf=10)),
     ('clf', linear_model.LinearRegression()),
-    # ('clf', MLPRegressor(hidden_layer_sizes=[2], verbose=True, max_iter=200, tol=0.001)),
     # ('clf', linear_model.Ridge(alpha=0.0015)),  # can drop PCA
 ])
 
@@ -108,15 +122,15 @@ r2 = r2_score(y_test, y_pred)
 
 print(f'MSE: {mse}')
 print(f'R-squared: {r2}')
-print(model.steps[2][1].coef_)
+print(model.steps[1][1].coef_)
 
 # ---------------- #
 
 # examine pca
 pca = PCA(20)
-pd.DataFrame(pca.fit_transform(preprocessing.StandardScaler().fit_transform(X_train)), index=X_train.index).iloc[:4, :].plot()
+pd.DataFrame(pca.fit_transform(X_train), index=X_train.index).iloc[:, :4].plot()
 plt.figure()
-plt.plot(pca.fit_transform(X_train))
+plt.plot(X_train)
 
 # random forest feature importance
 clf = RandomForestRegressor(n_estimators=5, min_samples_leaf=10)
--- ridge_feature_importance.py
+++ ridge_feature_importance.py
@@ -16,6 +16,7 @@ concentration = pd.read_csv('Concentration_set.csv')
 # define x and y
 y = concentration['concentration_lactate']
 spectra_ = spectra.drop('wavelength', axis=1).transpose()
+spectra_ = spectra_.div(spectra_.max(axis=1), axis=0)
 
 # split data
 X_train, X_test, y_train, y_test = train_test_split(spectra_, y, test_size=0.2, random_state=42)
@@ -28,6 +29,7 @@ exponential_series = np.exp(np.linspace(np.log(start), np.log(end), num_points))
 
 fig, ax = plt.subplots()
 spectra.set_index('wavelength').plot(legend=False, ax=ax)
+# (spectra_.T.set_index(spectra['wavelength']) * 10).plot(legend=False, ax=ax)
 for alpha in exponential_series:
 
     # define model
@@ -47,5 +49,5 @@ for alpha in exponential_series:
     min_value = np.min(model.steps[1][1].coef_)
     max_value = np.max(model.steps[1][1].coef_)
     importance = (model.steps[1][1].coef_ - min_value) / (max_value - min_value)
-    pd.Series(importance, index=spectra['wavelength']).plot(ax=ax, title=r2)
+    pd.Series(importance, index=spectra['wavelength']).plot(ax=ax, title=f"R2: {r2.round(2)}, alpha: {alpha.round(6)}")
     plt.pause(0.0001)
